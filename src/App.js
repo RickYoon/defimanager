776ada20f153dd94406ad3ea1be3684a9b45b552
RickYoon <yoon1515@gmail.com>
2022-01-20 20:48:08 +0900

    1st openning

--- src/App.js
+++ src/App.js
@@ -16,11 +16,33 @@ const TodoTemplateBlock = styled.div`
 
   margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
 
-  margin-top: 96px;
+  margin-top: 16px;
   margin-bottom: 32px;
   display: flex;
   flex-direction: column;
+  @media screen and (max-width: 500px){
+    width: 350px;
+  }
 `;
+
+const TemplateBlock = styled.div`
+  width: 512px;
+  max-height: 768px;
+
+  position: relative; /* 추후 박스 하단에 추가 버튼을 위치시키기 위한 설정 */
+  border-radius: 16px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+
+  margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
+
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  @media screen and (max-width: 500px){
+    width: 350px;
+  }
+`;
+
 
 
 const TodoHeadBlock = styled.div`
@@ -37,15 +59,19 @@ const TodoHeadBlock = styled.div`
   .day {
     margin-top: 4px;
     color: #868e96;
-    font-size: 15px;
+    font-size: 12px;
     float: right;
   }
+  .dayy {
+    margin-top: 4px;
+    font-size: 15px;
+  }
   .tasks-left {
-    color: #20c997;
     font-size: 20px;
     margin-top: 10px;
     font-weight: bold;
     padding-left: 10px;
+    font-weight: bold;
   }
 `;
 
@@ -56,6 +82,10 @@ function App() {
 
   const [tvldata, setTvldata] = useState({
     refDate: "0000-00-00",
+    total: {
+      tvl: 0,
+      diff: 0
+    },
     data: []
   })
 
@@ -67,35 +97,56 @@ function App() {
     const url = "https://uv8kd7y3w5.execute-api.ap-northeast-2.amazonaws.com/production/"
     await axios.get(url).then(function (response) {
       console.log(response.data.body.data)
-      let tempArr = response.data.body.data
+      let tempArr = response.data.body.data.filter(dat => dat.proj !== "KCT-Total")
+      let tempTotal = response.data.body.data.filter(dat => dat.proj === "KCT-Total")
 
       tempArr.sort(function (a, b) {
         return a.tvl > b.tvl ? -1 : a.tvl < b.tvl ? 1 : 0;
       })
       console.log(response)
+      console.log("tempTotal", tempTotal)
 
+      const responseObj = {
+        refDate: response.data.body.refDate,
+        total: tempTotal[0],
+        data: tempArr
+      }
 
-      setTvldata(response.data.body)
-      console.log("tvldata", tvldata)
+      setTvldata(responseObj)
     })
   }
+
+  const transnumber = () => {
+
+    return (
+      <>
+        {tvldata.total.tvl > 10000000 ?
+          <span> ${(tvldata.total.tvl / 1000000000).toFixed(1)}B</span> :
+          <span> under </span>
+        }
+      </>
+    )
+  }
+
+  // tvldata.total.tvl
+
 
   return (
     <>
       <GlobalStyles />
-
+      <TemplateBlock>KlayLabs (beta)</TemplateBlock>
       <TodoTemplateBlock>
         <TodoHeadBlock>
           <div>
-            <span className="tasks-left">TVL : $22B ( +7% / 7days )</span>
+            <span className="tasks-left">TVL:{transnumber()}</span> <span className="dayy">({tvldata.total.diff}%/7days)</span>
             <span className="day">ref: {tvldata.refDate}</span>
           </div>
         </TodoHeadBlock>
 
         <div style={{ margin: "20px" }}>
-          <table>
+          <table style={{ borderCollapse: "collapse", borderWidth: "1px" }}>
             <thead>
-              <tr style={{ height: "35px", border: "1em" }}>
+              <tr style={{ height: "35px" }}>
                 <th style={{ width: "50px" }}>Rank</th>
                 <td style={{ width: "200px", paddingLeft: "1em" }}>proj</td>
                 <td style={{ width: "100px", textAlign: "right" }}>TVL($)</td>
@@ -104,13 +155,16 @@ function App() {
             </thead>
             <tbody>
 
-              {tvldata.data.length == 0 ? <div>Loading</div> :
+              {tvldata.data.length === 0 ? <div>Loading</div> :
                 tvldata.data.map((tvld, index) => (
                   <tr style={{ height: "35px" }}>
                     <td style={{ width: "50px", textAlign: "center" }}>{index + 1}</td>
                     <td style={{ width: "200px", paddingLeft: "1em" }}>{tvld.proj}</td>
                     <td style={{ width: "100px", textAlign: "right" }}>{tvld.tvl.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
-                    <td style={{ width: "100px", textAlign: "right" }}>{tvld.diff}%</td>
+                    {tvld.diff > 0 ?
+                      <td style={{ width: "100px", textAlign: "right", color: "blue" }}>{tvld.diff}%</td> :
+                      <td style={{ width: "100px", textAlign: "right", color: "red" }}>{tvld.diff}%</td>
+                    }
                   </tr>
                 ))
               }
